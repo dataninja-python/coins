85865f6e0ba0256bfeed185e35bc39b0e3b1f9f1
ajigherighe <aj@redstonema.com>
2021-10-18 15:35:06 -0400

    reworking as recursive graph data structure

--- main.go
+++ main.go
@@ -13,12 +13,13 @@ func timeTrack(start time.Time, name string) {
 }
 
 func execute() int {
-	defer timeTrack(time.Now(), "coins1")
 	change := []int{5, 7, 1, 1, 2, 3, 22}
+	// change := []int{}
 	minChange := nonconstructible.NonConstructibleChange(change)
 	return minChange
 }
 
 func main() {
-	fmt.Println(execute())
+	defer timeTrack(time.Now(), "coins1")
+	fmt.Println("Minimum Number: ", execute())
 }
--- nonconstructible/nonconstructible.go
+++ nonconstructible/nonconstructible.go
@@ -5,19 +5,38 @@ import (
 	"sort"
 )
 
-func NonConstructibleChange(coins []int) int {
-	minChange := -1
-	sort.Ints(coins)
-	fmt.Println(minChange)
-	total := sumAll(coins)
-	fmt.Println(total)
-	return coins[0]
+func permutation(coinsArr []int) (permuts [][]int) {
+	var rc func([]int, int)
+	rc = func(a []int, k int) {
+		if k == len(a) {
+			permuts = append(permuts, append([]int{}, a...))
+		} else {
+			for i := k; i < len(coinsArr); i++ {
+				a[k], a[i] = a[i], a[k]
+				rc(a, k+1)
+				a[k], a[i] = a[i], a[k]
+			}
+		}
+	}
+	rc(coinsArr, 0)
+
+	return permuts
 }
 
-func sumAll(coins []int) int {
-	aSum := 0
-	for _, v := range coins {
-		aSum += v
+func NonConstructibleChange(coins []int) int {
+	// create minimum change variable
+	// initialize it with 1
+	minChange := 1
+	// if no coins provided return 1
+	if len(coins) == 0 {
+		return 1
 	}
-	return aSum
+	sort.Ints(coins)
+	// process elements from left to right
+	noOfPerms := permutation(coins)
+	fmt.Println("Number of Permutation:", len(noOfPerms))
+	for i := 0; i < len(noOfPerms); i++ {
+		fmt.Println(noOfPerms[i])
+	}
+	return minChange
 }
