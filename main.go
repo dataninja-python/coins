53da8b4b61aaabd2404e0aa3669ffb2b0281badb
ajigherighe <aj@redstonema.com>
2021-10-20 17:19:04 -0400

    recursive approach crashes on large inputs

--- main.go
+++ main.go
@@ -14,12 +14,25 @@ func timeTrack(start time.Time, name string) {
 
 func execute() int {
 	change := []int{5, 7, 1, 1, 2, 3, 22}
+	// change := []int{1, 1, 1, 1, 1}
+	// change := []int{1, 5, 1, 1, 1, 10, 15, 20, 100}
+	// change := []int{6, 4, 5, 1, 1, 8, 9}
 	// change := []int{}
-	minChange := nonconstructible.NonConstructibleChange(change)
-	return minChange
+	// change := []int{87}
+	// change := []int{5, 6, 1, 1, 2, 3, 4, 9}
+	// change := []int{5, 6, 1, 1, 2, 3, 43}
+	// change := []int{1, 1}
+	// change := []int{2}
+	// change := []int{1}
+	// change := []int{109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4}
+	// change := []int{}
+	// minChange := nonconstructible.NonConstructibleChange(change)
+	minChange2 := nonconstructible.NonConstructibleChange2(change)
+	fmt.Println(minChange2)
+	return minChange2
 }
 
 func main() {
 	defer timeTrack(time.Now(), "coins1")
-	fmt.Println("Minimum Number: ", execute())
+	fmt.Println("Minimum change not constructible: ", execute())
 }
