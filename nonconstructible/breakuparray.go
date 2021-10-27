c57aade23898472a51fb0dbb5f1da122b92ac9df
ajigherighe <aj@redstonema.com>
2021-10-27 12:16:20 -0400

    refactoring to brute force method

--- nonconstructible/breakuparray.go
+++ nonconstructible/breakuparray.go
@@ -5,20 +5,43 @@ import (
 	"sort"
 )
 
-func WalkArray(arr []int) {
-	sort.Ints(arr)
-	temp := []int{}
-	// tempMap := map[int][][]int{}
-	// row := 1
-	// you know you are going to do something for the length of the array
-	for i, v := range arr {
-		fmt.Println("The current index: ", i)
-		fmt.Println("The current value: ", v)
-		if i+1 == len(arr)+1 {
-			continue
-		} else {
-			temp = arr[i+1:]
+/*type comparison struct {
+	testNum    int
+	largestSum int
+	startInd   int
+	testArr    []int
+}*/
+
+func canCreateChange(testNum int, numArr []int) bool {
+	theSum := 0
+	for _, v := range numArr {
+		theSum += v
+		// if we can create the change return true
+		if testNum == theSum {
+			return true
 		}
-		fmt.Println("The current short array: ", temp)
+		// if the sum of numbers in array is greater than the testNum
+		// we can not create the change
+		if theSum > testNum {
+			return false
+		}
 	}
+	return false
+}
+
+func WalkArray(arr []int) bool {
+	sort.Ints(arr)
+	fmt.Println("Sorted array: ", arr)
+	currentChange := 0
+	continueCount := true
+
+	for continueCount {
+		// increment to create first change for comparison
+		currentChange++
+		if currentChange == 10 {
+			continueCount = false
+		}
+	}
+	fmt.Println("Change can't create: ", currentChange)
+	return false
 }
