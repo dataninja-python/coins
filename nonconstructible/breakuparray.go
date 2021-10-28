0fa8fa39980f725a4e9c210205ef935cf2bbea42
ajigherighe <aj@redstonema.com>
2021-10-28 11:02:05 -0400

    another attempt to loop through array

--- nonconstructible/breakuparray.go
+++ nonconstructible/breakuparray.go
@@ -11,19 +11,27 @@ import (
 	startInd   int
 	testArr    []int
 }*/
-
-func canCreateChange(testNum int, numArr []int) bool {
-	theSum := 0
-	for _, v := range numArr {
-		theSum += v
-		// if we can create the change return true
-		if testNum == theSum {
-			return true
+func permut(t []int, p [][]int) [][]int {
+	var rc func([]int, int)
+	rc = func(a []int, k int) {
+		if k == len(a) {
+			p = append(p, append([]int{}, a...))
+		} else {
+			for i := k; i < len(t); i++ {
+				a[k], a[i] = a[i], a[k]
+				rc(a, k+1)
+				a[k], a[i] = a[i], a[k]
+			}
 		}
-		// if the sum of numbers in array is greater than the testNum
-		// we can not create the change
-		if theSum > testNum {
-			return false
+	}
+	rc(t, 0)
+	return p
+}
+
+func canCreateSingle(testNum int, numArr []int) bool {
+	for _, v := range numArr {
+		if testNum == v {
+			return true
 		}
 	}
 	return false
@@ -34,13 +42,26 @@ func WalkArray(arr []int) bool {
 	fmt.Println("Sorted array: ", arr)
 	currentChange := 0
 	continueCount := true
+	// tempArr := []int{}
 
 	for continueCount {
 		// increment to create first change for comparison
 		currentChange++
-		if currentChange == 10 {
-			continueCount = false
+		var permutations [][]int
+		// can create current number from a single loop through the array
+		// continueCount = canCreateSingle(currentChange, arr)
+		permutations = permut(arr, permutations)
+		for _, a := range permutations {
+			theSum := 0
+			for _, v := range a {
+				theSum = v
+				fmt.Println(theSum)
+				if currentChange == theSum {
+					continue
+				}
+			}
 		}
+		continueCount = false
 	}
 	fmt.Println("Change can't create: ", currentChange)
 	return false
