7ac3670cc1b7f65880749f815a7d7b1857456e89
ajigherighe <aj@redstonema.com>
2021-11-02 15:03:18 -0400

    refactoring and adding test cases

---
+++ nonconstructible/change_test.go
@@ -0,0 +1,37 @@
+package nonconstructible
+
+import (
+	"testing"
+)
+
+// source: https://www.digitalocean.com/community/tutorials/how-to-write-unit-tests-in-go-using-go-test-and-the-testing-package
+type changeTest struct {
+	slice    []int
+	expected int
+}
+
+var changeTests = []changeTest{
+	changeTest{[]int{1, 2, 5}, 4},
+	changeTest{[]int{}, 1},
+	changeTest{[]int{87}, 1},
+	changeTest{[]int{5, 7, 1, 1, 2, 3, 22}, 20},
+	changeTest{[]int{1, 1, 1, 1, 1}, 5},
+	changeTest{[]int{1, 5, 1, 1, 1, 10, 15, 20, 100}, 45},
+	changeTest{[]int{6, 4, 5, 1, 1, 8, 9}, 3},
+	changeTest{[]int{5, 6, 1, 1, 2, 3, 4, 9}, 32},
+	changeTest{[]int{5, 6, 1, 1, 2, 3, 43}, 19},
+	changeTest{[]int{1, 1}, 3},
+	changeTest{[]int{2}, 1},
+	changeTest{[]int{1}, 2},
+	changeTest{[]int{109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4}, 87},
+	changeTest{[]int{1, 2, 3, 4}, 11},
+}
+
+func TestChange(t *testing.T) {
+	for _, test := range changeTests {
+		if output := NonConstructibleChange(slice); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d",
+				output, test.expected)
+		}
+	}
+}
--- nonconstructible/nonconstructible.go
+++ nonconstructible/nonconstructible.go
@@ -6,12 +6,13 @@ import (
 )
 
 type workVars struct {
-	changeMakeable bool
-	swaps          int
-	workSlice      []int
-	sumMap         map[int]bool
-	sumSlice       []int
-	originalSlice  []int
+	changeMakeable     bool
+	swaps              int
+	workSlice          []int
+	sumMap             map[int]bool
+	sumSlice           []int
+	originalSlice      []int
+	lastChangeMakeable int
 }
 
 func getNewSlice(originalSlice []int) []int {
@@ -28,13 +29,66 @@ func min(x, y int) int {
 	return y
 }
 
+func getCutSlice(aSlice []int, startInc int, stopExc int) []int {
+	temp := []int{}
+	// skip part to be removed
+	for index, value := range aSlice {
+		if index >= startInc && index < stopExc {
+			continue
+		} else {
+			temp = append(temp, value)
+		}
+	}
+	return temp
+}
+
+func getMaxChangeMakeable(theWorker workVars) workVars {
+	sort.Ints(theWorker.sumSlice)
+	theWorker.changeMakeable = true
+	counter := 0
+	for i := 0; i < len(theWorker.sumSlice); i++ {
+		counter++
+		fmt.Println(counter, " :counter and value: ", theWorker.sumSlice[i])
+	}
+	return theWorker
+}
+
 func getSumsPerSlice(startInc, stopExc int, aWorker workVars) workVars {
+	// create a new temporary slice for this function
+	tempSlice := getNewSlice(aWorker.workSlice)
+	// slice to sum before going through loop of each item
+	baseSumSlice := aWorker.workSlice[startInc:stopExc]
+	// add base sum to each item in this slice
+	// NOTE: this is the slice that does not contained items added to baseSum
+	remainingSlice := getCutSlice(tempSlice, startInc, stopExc)
+
+	baseSum := 0
+	fmt.Println("Starting base sum: ", baseSum)
+	for _, value := range baseSumSlice {
+		baseSum += value
+		if _, ok := aWorker.sumMap[baseSum]; !ok {
+			fmt.Println(baseSum, " int added to sum")
+			aWorker.sumMap[baseSum] = true
+			aWorker.sumSlice = append(aWorker.sumSlice, baseSum)
+		}
+	}
+	fmt.Println("Interim base sum: ", baseSum)
+	for i := 0; i < len(remainingSlice); i++ {
+		baseSum += remainingSlice[i]
+		if _, ok := aWorker.sumMap[baseSum]; !ok {
+			fmt.Println(baseSum, " int added to basesum")
+			aWorker.sumMap[baseSum] = true
+			aWorker.sumSlice = append(aWorker.sumSlice, baseSum)
+		}
+		baseSum -= remainingSlice[i]
+	}
+	aWorker = getMaxChangeMakeable(aWorker)
 	return aWorker
 }
 
 func getUniqueMakeableChange(begIndex int, begSpread int,
 	aWorker workVars) workVars {
-	for spread := begSpread; spread < len(aWorker.originalSlice); spread++ {
+	for spread := begSpread; spread <= len(aWorker.originalSlice); spread++ {
 		for sliceIndex := begIndex; sliceIndex < len(aWorker.originalSlice); sliceIndex++ {
 			// create variables to hold looping through each combination of
 			// the sorted slice to get all sums
@@ -42,8 +96,8 @@ func getUniqueMakeableChange(begIndex int, begSpread int,
 			// must protect against going outside the bounds of slice exclusive
 			// this should be len(slice)
 			endSliceExclusive := min(startSliceInclusive+spread, len(aWorker.originalSlice))
-			fmt.Println("start include: ", startSliceInclusive,
-				"stop exclude: ", endSliceExclusive)
+			aWorker = getSumsPerSlice(startSliceInclusive,
+				endSliceExclusive, aWorker)
 		}
 	}
 	return aWorker
@@ -65,11 +119,12 @@ func NonConstructibleChange(coins []int) int {
 	s1 := getNewSlice(coins)
 	s2 := getNewSlice(coins)
 	sumTracker := make(map[int]bool)
-	worker := workVars{changeMakeable: true, swaps: 0, workSlice: s1, sumMap: sumTracker, originalSlice: s2}
-	fmt.Println("Initial worker workVars: ", worker)
+	worker := workVars{changeMakeable: true, swaps: 0, workSlice: s1, sumMap: sumTracker, originalSlice: s2, lastChangeMakeable: minChange}
+	fmt.Println("Initial worker workVars: ", worker, "\n")
 	// start finding all unique change combinations using an inner loop
 	// to loop through eah index in a coin and a spread to extract the
 	// portion of the coin array to count
 	worker = getUniqueMakeableChange(0, 1, worker)
+	fmt.Println("End worker workVars: ", worker, "\n")
 	return minChange
 }
