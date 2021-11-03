da910529a7cf4a08b346c7fe144e10ad64578ea4
ajigherighe <aj@redstonema.com>
2021-11-02 21:53:41 -0400

    rewriting because I realize I am not capturing the middle combinations

--- minchangetable_test.go
+++ minchangetable_test.go
@@ -17,7 +17,7 @@ var changeTests = []changeTest{
 	changeTest{[]int{87}, 1},
 	changeTest{[]int{5, 7, 1, 1, 2, 3, 22}, 20},
 	changeTest{[]int{1, 1, 1, 1, 1}, 6},
-	changeTest{[]int{1, 5, 1, 1, 1, 10, 15, 20, 100}, 45},
+	changeTest{[]int{1, 5, 1, 1, 1, 10, 15, 20, 100}, 55},
 	changeTest{[]int{6, 4, 5, 1, 1, 8, 9}, 3},
 	changeTest{[]int{5, 6, 1, 1, 2, 3, 4, 9}, 32},
 	changeTest{[]int{5, 6, 1, 1, 2, 3, 43}, 19},
@@ -26,6 +26,7 @@ var changeTests = []changeTest{
 	changeTest{[]int{1}, 2},
 	changeTest{[]int{109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4}, 87},
 	changeTest{[]int{1, 2, 3, 4}, 11},
+	changeTest{[]int{1, 2, 3, 4, 5, 6, 7}, 29},
 }
 
 func TestChange(t *testing.T) {
